e318333f752541dbecdefca07cd3080f5957059e
bcfolsom <bcfolsom@gmail.com>
2017-04-25 21:08:29 -0500

    commit 4

--- io_ex1.py
+++ io_ex1.py
@@ -1,13 +1,10 @@
 #1 Write a program that prompts the user to enter a file name, reads the contents of the file and prints it to the screen.
 
 file_name = input('Enter file name: ')
-
 def i (file_name):
-
     file_handle = open(file_name, 'r')
     contents = file_handle.read()
     file_handle.close()
-
     print(contents)
 
 i(file_name)
--- io_ex3.py
+++ io_ex3.py
@@ -1,28 +1,25 @@
-# 3 Write a program that prompts the user to enter a file name, then prints the
-# letter histogram and the word histogram of the contents of the file.
-
-file_name = input('Give me a file name to write: ')
-contents = input('Enter what you would like to write: ')
+file_name = input('Enter a file name: ')
 def x (file_name):
+    file_handle = open(file_name, 'r')
+file=open(file_name, 'r')
+wordcount = {}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+print (word,wordcount)
+file.close();
 
-    file_handle = open(file_name, 'w')
-    file_handle.write(contents)
-    file_handle.close()
-
-x(file_name)
-
-dic = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
+dic = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0, ' ': 0, '.': 0, '\n': 0}
 dic2 = {}
-
-word = input('Give me a word: ').lower()
-def letter(word):
-    for x in word:
+file=open(file_name, 'r')
+words = file.read()
+def letter(words):
+    for x in words:
         dic[x] = dic[x]+1
-
     for key, value in dic.items():
         if value != 0:
             dic2[key] = value
-
     print(dic2)
-
-letter(word)
+letter(words)
